abf2f0b33a8b08a852e1111311f8d8ddd17e1526
eijisuda <suda@wizgeek.co.jp>
2021-10-14 18:03:10 +0900

    vue add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+      cursor: ['disabled'],
+    },
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
