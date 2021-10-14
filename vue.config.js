63007c97a3b92f36cf1c768621bda0e2602a4e9e
eijisuda <suda@wizgeek.co.jp>
2021-10-14 17:11:05 +0900

    vue add i18n

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,17 @@ module.exports = {
     disableHostCheck: true,
     host: '0.0.0.0',
     port: 3000
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'ja',
+      fallbackLocale: 'ja',
+      localeDir: 'locales',
+      enableLegacy: false,
+      runtimeOnly: false,
+      compositionOnly: false,
+      fullInstall: true
+    }
   }
 }
